11a35f4e7c77a65d652bc4a34e0ce8d055091e75
Mikail Russbell Castro Jara <crussbell@gmail.com>
2022-02-26 11:25:54 -0500

    mensaje.js edit

--- helpers/mensajes.js
+++ helpers/mensajes.js
@@ -5,6 +5,11 @@ const mostrarMenu = () => {
 	console.log("=========================================".green);
 	console.log("        Seleccione una opción".green);
 	console.log("=========================================\n".green);
+	console.log(`1. Crear tareas`);
+	console.log(`2. Listar tareas`);
+	console.log(`2. Listar tareas completadas`);
+	console.log(`2. Listar tareas Pendientes`);
+	console.log(`2. Completar tareas(s)`);
 };
 
 module.exports = { mostrarMenu };
