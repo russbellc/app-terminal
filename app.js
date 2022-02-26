8cb549d7976c01cb27673411e8dc28c5c5ebd18f
Mikail Russbell Castro Jara <russbellc@gmail.com>
2022-02-26 11:15:01 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,10 @@
+require("colors");
+
+const { mostrarMenu } = require("./helpers/mensajes");
+
+const main = async () => {
+	console.log("hola mundo");
+	mostrarMenu();
+};
+
+main();
---
+++ helpers/mensajes.js
@@ -0,0 +1,10 @@
+require("colors");
+
+const mostrarMenu = () => {
+	console.clear();
+	console.log("=========================================".green);
+	console.log("        Seleccione una opción".green);
+	console.log("=========================================\n".green);
+};
+
+module.exports = { mostrarMenu };
